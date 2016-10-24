798bc1c142349578f953356e8f78ab2e35527c1a
wkomer <euro_driver@live.com>
2016-10-24 15:08:31 -0400

    Updated the logo on the navbars.

--- js/waypoints.js
+++ js/waypoints.js
@@ -6,7 +6,7 @@ console.log('waypoint');
 
 
 if (direction =='down'){
-  $("#socialmedia").prepend("<div id='divnav' class='navbar-fixed-top sec-nav rev-nav'><div class='navbar-inner'><div class='container'><div class='navbar-header'><a class='navbar-brand logo'>LOGO</a></div><div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav navbar-right'><li><a class='about' href='#hero-unit'>About</a></li><li><a class='socialmedia' href='#socialmedia'>Social Media</a></li><li><a class='contact' href='#contact'>Contact</a></li></ul></div></div></div></div>");
+  $("#socialmedia").prepend("<div id='divnav' class='navbar-fixed-top sec-nav rev-nav'><div class='navbar-inner'><div class='container'><div class='navbar-header'><a class='navbar-brand logo'><img class='img' src='/images/DSCI1.png'></a></div><div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav navbar-right'><li><a class='about' href='#hero-unit'>About</a></li><li><a class='socialmedia' href='#socialmedia'>Social Media</a></li><li><a class='contact' href='#contact'>Contact</a></li></ul></div></div></div></div>");
 
 }
 if(direction == 'up'){
