a9d95d7c7f7ff7c18cfec9b465f22691e1943bd7
wkomer <euro_driver@live.com>
2016-10-24 13:22:37 -0400

    First Rough Pass, Need Main hero image, Need Logo, Need more SM

---
+++ js/waypoints.js
@@ -0,0 +1,18 @@
+var $nav = $('#socialmedia');
+
+$nav.waypoint(function(direction){
+console.log('waypoint');
+
+
+
+if (direction =='down'){
+  $("#socialmedia").prepend("<div id='divnav' class='navbar-fixed-top sec-nav rev-nav'><div class='navbar-inner'><div class='container'><div class='navbar-header'><a class='navbar-brand logo'>LOGO</a></div><div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav navbar-right'><li><a class='about' href='#hero-unit'>About</a></li><li><a class='socialmedia' href='#socialmedia'>Social Media</a></li><li><a class='contact' href='#contact'>Contact</a></li></ul></div></div></div></div>");
+
+}
+if(direction == 'up'){
+  $("#divnav").remove();
+}
+
+$("#divnav").addClass('sec-nav-opacity1');
+},
+{offset:'10%'});
